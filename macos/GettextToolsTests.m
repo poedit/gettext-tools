24589aed2740f02f284e5e0843cf74fa4e4f6666
Václav Slavík <vaclav@slavik.io>
2026-06-13 16:24:43 +0200

    Xcode: add barebones test suite

---
+++ macos/GettextToolsTests.m
@@ -0,0 +1,37 @@
+//
+//  GettextToolsTests.m
+//  GettextToolsTests
+//
+//  Created by Václav Slavík on 13.06.2026.
+//  Copyright © 2026 Václav Slavík. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GettextToolsTests : XCTestCase
+
+@end
+
+@implementation GettextToolsTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
