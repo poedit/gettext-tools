a08821e396c0dd2bb0c867bfbbf3b3e92885f651
Václav Slavík <vaclav@slavik.io>
2026-06-16 17:52:24 +0200

    Publish 1.0 packages

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GettextTools",
-            url: "https://download.poedit.com/tmp/GettextTools-1.0-2.xcframework.zip",
-            checksum: "7bfa5a1efb2fd903fc8f65bf699bb4a81766feedae9183cac595d63652d526f2"
+            url: "https://github.com/poedit/gettext-tools/releases/download/v1.0/GettextTools.xcframework.zip",
+            checksum: "04dcba5b92ad8d0ff475e29baee72180f096431af220a858f0d29a9ec8988dcd"
         ),
     ]
 )
